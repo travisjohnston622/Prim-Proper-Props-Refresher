d977f6f589c8219844897d216c2aadf4f8a8ead6
Travis Johnston <travisjohnston4588@icloud.com>
2020-08-17 18:25:57 -0500

    Header.js functionality complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>Prim Proper Props</h1>
+            </header>
+        );
+    }
+}
+
+export default Header;
